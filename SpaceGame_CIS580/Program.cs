7d1cc19df8f62b4f59cc264bbfd41931df78a4c4
Brandon Bednar <bwbednar@ksu.edu>
2022-02-09 17:33:29 -0600

    initial commit

---
+++ SpaceGame_CIS580/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpaceGame_CIS580
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new SpaceGame())
+                game.Run();
+        }
+    }
+}
