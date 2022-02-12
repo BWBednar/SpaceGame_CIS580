52b092c1758bf5ff88b9409d13df04bc1df52199
Brandon B <bwbednar@ksu.edu>
2022-02-11 18:22:55 -0600

    got asteroids showing

---
+++ SpaceGame_CIS580/AsteroidSprite.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace SpaceGame_CIS580
+{
+    public class AsteroidSprite
+    {
+        private Texture2D _texture;
+        private Vector2 origin;
+        private float radius;
+        private float scale;
+
+        /// <summary>
+        /// Indicates if the asteroid has collided with anything
+        /// </summary>
+        public bool Colliding { get; set; }
+
+        /// <summary>
+        /// Vector for the center of the asteroid
+        /// </summary>
+        public Vector2 Center { get; set; }
+
+        /// <summary>
+        /// Vector for the velocity of the asteroid
+        /// </summary>
+        public Vector2 Velocity { get; set; }
+
+        public float Mass
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                scale = radius / 16;
+                origin = new Vector2(16, 16); // asteroid sprite is 32 by 32 pixels
+            }
+        }
+
+        /// <summary>
+        /// Loads the image the asteroid sprite is contained in
+        /// </summary>
+        /// <param name="contentManager">The game's content manager</param>
+        public void LoadContent(ContentManager contentManager)
+        {
+            _texture = contentManager.Load<Texture2D>("CelestialObjects");
+        }
+
+        /// <summary>
+        /// Updates the movement of the asteroid
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            Center += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //Keeps the asteroid on screen, will eventually want it to travel to the opposite end of the screen
+            if (Center.X < radius || Center.X > Constants.GAME_WIDTH - radius) Velocity *= -Vector2.UnitX;
+            if (Center.Y < radius || Center.Y > Constants.GAME_HEIGHT - radius) Velocity *= -Vector2.UnitY;
+
+            Colliding = false;
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            Rectangle source = new Rectangle(0, 192, 32, 32);
+            spriteBatch.Draw(_texture, Center, source, Color.White, 0, origin, scale, SpriteEffects.None, 0);
+        }
+
+        public bool CollidesWith(AsteroidSprite other)
+        {
+            return this.Mass + other.Mass >= Vector2.Distance(Center, other.Center);
+        }
+    }
+
+}
--- SpaceGame_CIS580/SpaceGame.cs
+++ SpaceGame_CIS580/SpaceGame.cs
@@ -2,12 +2,13 @@
  * Starting Code from Nathan Bean's GameArchitectureExample project
  */
 
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SpaceGame_CIS580.Screens;
 using SpaceGame_CIS580.StateManagement;
-using tainicom.Aether.Physics2D.Dynamics;
+//using tainicom.Aether.Physics2D.Dynamics; Was having trouble with physics engine, so following example for the example C for game physics for now
 
 namespace SpaceGame_CIS580
 {
@@ -18,8 +19,10 @@ namespace SpaceGame_CIS580
     public class SpaceGame : Game
     {
         private GraphicsDeviceManager _graphics;
-        private readonly ScreenManager _screenManager;
-        private World world;
+        private SpriteBatch _spriteBatch;
+        //private readonly ScreenManager _screenManager;
+        //private World world;
+        List<AsteroidSprite> _asteroids;
 
         public SpaceGame()
         {
@@ -30,19 +33,22 @@ namespace SpaceGame_CIS580
             _graphics.PreferredBackBufferHeight = Constants.GAME_HEIGHT;
             _graphics.ApplyChanges();
 
-            var screenFactory = new ScreenFactory();
-            Services.AddService(typeof(IScreenFactory), screenFactory);
 
-            _screenManager = new ScreenManager(this);
-            Components.Add(_screenManager);
+            //Going to implement actual game architecture later, going to go simple version for functionality first
 
-            AddInitialScreens();
+            //var screenFactory = new ScreenFactory();
+            //Services.AddService(typeof(IScreenFactory), screenFactory);
+
+            //_screenManager = new ScreenManager(this);
+            //Components.Add(_screenManager);
+
+            //AddInitialScreens();
         }
 
         private void AddInitialScreens()
         {
-            _screenManager.AddScreen(new BackgroundScreen(), null);
-            _screenManager.AddScreen(new GamePlayScreen(), null);
+            //_screenManager.AddScreen(new BackgroundScreen(), null);
+            //_screenManager.AddScreen(new GamePlayScreen(), null);
             //_screenManager.AddScreen(new BackgroundScreen(), null);
             //_screenManager.AddScreen(new MainMenuScreen(), null);
             //_screenManager.AddScreen(new SplashScreen(), null);
@@ -50,8 +56,36 @@ namespace SpaceGame_CIS580
 
         protected override void Initialize()
         {
+            System.Random random = new System.Random();
+            _asteroids = new List<AsteroidSprite>();
+            int xMidPoint = Constants.GAME_WIDTH / 2;
+            int yMidPoint = Constants.GAME_HEIGHT / 2;
+            for (int i = 0; i < 10; i++)
+            {
+                int xLocation = random.Next(50, Constants.GAME_WIDTH - 50);
+                int yLocation = random.Next(50, Constants.GAME_HEIGHT - 50);
+                while ((xLocation >= xMidPoint + 150 || xLocation <= xMidPoint - 150) ||
+                    ((yLocation >= yMidPoint + 150 || yLocation <= yMidPoint - 150)))
+                {
+                    _asteroids.Add(new AsteroidSprite()
+                    {
+                        Center = new Vector2(xLocation, yLocation),
+                        Velocity = new Vector2(50 - (float)random.NextDouble() * 100, 50 - (float)random.NextDouble() * 100),
+                        Mass = 25
+                    });
+                    xLocation = random.Next(50, Constants.GAME_WIDTH - 50);
+                    yLocation = random.Next(50, Constants.GAME_HEIGHT - 50);
+                }
+
+            }
+
+            base.Initialize();
+            //asteroids = new List
+
+            /*
             base.Initialize();
 
+            
             world = new World();
             world.Gravity = Vector2.Zero;
             var top = 0;
@@ -70,20 +104,36 @@ namespace SpaceGame_CIS580
                 edge.BodyType = BodyType.Static;
                 edge.SetRestitution(1.0f);
             }
+            */
 
         }
 
-        protected override void LoadContent() { }
+        protected override void LoadContent() 
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            foreach (var asteroid in _asteroids) asteroid.LoadContent(Content);
+        }
 
         protected override void Update(GameTime gameTime)
         {
+            //base.Update(gameTime);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            foreach (var asteroid in _asteroids) asteroid.Update(gameTime);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            base.Draw(gameTime);    // The real drawing happens inside the ScreenManager component
+            //base.Draw(gameTime);    // The real drawing happens inside the ScreenManager component, not implemented yet
+            _spriteBatch.Begin();
+            foreach (var asteroid in _asteroids) asteroid.Draw(gameTime, _spriteBatch);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+
         }
     }
 }
