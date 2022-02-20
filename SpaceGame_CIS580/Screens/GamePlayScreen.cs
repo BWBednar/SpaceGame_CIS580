6ac5ae1513dde17253e2a8307a894c97b79214d2
Brandon Bednar <bwbednar@ksu.edu>
2022-02-19 21:25:29 -0600

    Got gameplay screen showing. need to have the loading screen disappear and then get updates for the gameplay screen handled

--- SpaceGame_CIS580/Screens/GamePlayScreen.cs
+++ SpaceGame_CIS580/Screens/GamePlayScreen.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SpaceGame_CIS580.StateManagement;
+using SpaceGame_CIS580.Sprites;
+using SpaceGame_CIS580.Collisions;
 
 namespace SpaceGame_CIS580.Screens
 {
@@ -16,9 +19,15 @@ namespace SpaceGame_CIS580.Screens
         private ContentManager _content;
         private SpriteFont _gameFont;
 
-        private Vector2 _playerPosition = new Vector2(100, 100);
-        //private SpaceshipSprite _ship = new SpaceshipSprite();
+        private SpaceshipSprite ship;
+        private List<AsteroidSprite> asteroids;
+        private List<BlasterFireSprite> blasterFire;
+        private List<ExplosionSprite> explosions;
 
+        private double timer;
+        private bool gameStart = true;
+        private bool gameLose = false;
+        private bool gameVictory = false;
         private float _pauseAlpha;
         private readonly InputAction _pauseAction;
 
@@ -27,15 +36,53 @@ namespace SpaceGame_CIS580.Screens
             TransitionOnTime = TimeSpan.FromSeconds(1.5);
             TransitionOffTime = TimeSpan.FromSeconds(0.5);
 
+            _pauseAction = new InputAction(
+                new[] { Buttons.Start, Buttons.Back },
+                new[] { Keys.Back, Keys.Escape }, true);
 
+            
         }
 
         public override void Activate()
         {
+            
+
+            //Get 10 asteroids in random positions around the ship sprite
+            System.Random random = new System.Random();
+            asteroids = new List<AsteroidSprite>();
+            int xMidPoint = Constants.GAME_WIDTH / 2;
+            int yMidPoint = Constants.GAME_HEIGHT / 2;
+            for (int i = 0; i < 10; i++)
+            {
+                int xLocation = random.Next(50, Constants.GAME_WIDTH - 50);
+                int yLocation = random.Next(50, Constants.GAME_HEIGHT - 50);
+                while ((xLocation <= xMidPoint + 150 && xLocation >= xMidPoint - 150) &&
+                    ((yLocation <= yMidPoint + 150 && yLocation >= yMidPoint - 150)))
+                {
+
+                    xLocation = random.Next(50, Constants.GAME_WIDTH - 50);
+                    yLocation = random.Next(50, Constants.GAME_HEIGHT - 50);
+                }
+                asteroids.Add(new AsteroidSprite()
+                {
+                    Center = new Vector2(xLocation, yLocation),
+                    Velocity = new Vector2(50 - (float)random.NextDouble() * 50, 50 - (float)random.NextDouble() * 50),
+                    Mass = 25
+                });
+            }
+            //Establish the other needed sprites
+            ship = new SpaceshipSprite(ScreenManager.Game, new BoundingRectangle(xMidPoint - 26, yMidPoint - 26, 52, 52));
+            blasterFire = new List<BlasterFireSprite>();
+            explosions = new List<ExplosionSprite>();
+
             if (_content == null) 
                 _content = new ContentManager(ScreenManager.Game.Services, "Content");
 
             _gameFont = _content.Load<SpriteFont>("PressStart2P");
+            foreach (var asteroid in asteroids) asteroid.LoadContent(_content);
+            ship.LoadContent(_content);
+
+           
 
             // A real game would probably have more content than this sample, so
             // it would take longer to load. We simulate that by delaying for a
@@ -62,12 +109,19 @@ namespace SpaceGame_CIS580.Screens
         {
             base.Update(gameTime, otherScreenHasFocus, false);
 
+            // Gradually fade in or out depending on whether we are covered by the pause screen.
+            if (coveredByOtherScreen)
+                _pauseAlpha = Math.Min(_pauseAlpha + 1f / 32, 1);
+            else
+                _pauseAlpha = Math.Max(_pauseAlpha - 1f / 32, 0);
+
             if (IsActive)
             {
                 //put gameplay here
             }
         }
 
+        //NEEDS CHANGES
         public override void HandleInput(GameTime gameTime, InputState input)
         {
             if (input == null)
@@ -97,19 +151,134 @@ namespace SpaceGame_CIS580.Screens
             if (movement.Length() > 1)
                 movement.Normalize();
 
-            _playerPosition += movement * 8f;
+            //_playerPosition += movement * 8f;
         }
 
         public override void Draw(GameTime gameTime)
         {
-            var spriteBatch = ScreenManager.SpriteBatch;
+            var _spriteBatch = ScreenManager.SpriteBatch;
+            _spriteBatch.Begin();
+            foreach (var asteroid in asteroids) if (!asteroid.Destroyed) asteroid.Draw(gameTime, _spriteBatch);
+            foreach (var fire in blasterFire) if (fire.OnScreen) fire.Draw(gameTime, _spriteBatch);
+            if (!ship.Destroyed) ship.Draw(gameTime, _spriteBatch);
+            foreach (var explosion in explosions) if (!explosion.AnimationComplete) explosion.Draw(gameTime, _spriteBatch);
+            //Display a message at the start of the game
+            if (gameStart)
+            {
+                _spriteBatch.DrawString(ScreenManager.Font, "Destory", new Vector2(75, 35), Color.Gold);
+                _spriteBatch.DrawString(ScreenManager.Font, "Asteroids", new Vector2(115, 85), Color.Gold);
+                timer += gameTime.ElapsedGameTime.TotalSeconds;
+                if (timer >= 3.0)
+                {
+                    gameStart = false;
+                }
+            }
+            //Display a message if the player loses the game
+            if (gameLose)
+            {
+                _spriteBatch.DrawString(ScreenManager.Font, "Restart Game", new Vector2(75, 35), Color.Gold);
+                _spriteBatch.DrawString(ScreenManager.Font, "To Try Again", new Vector2(115, 85), Color.Gold);
+            }
+            //Display a message if the player wins the game
+            if (gameVictory && !gameLose)
+            {
+                _spriteBatch.DrawString(ScreenManager.Font, "You Win!", new Vector2(75, 35), Color.Gold);
+                _spriteBatch.DrawString(ScreenManager.Font, "Well Done!", new Vector2(115, 85), Color.Gold);
+            }
+            _spriteBatch.End();
 
-            //spriteBatch.Begin();
-            //_ship.Draw(gameTime, spriteBatch);
-            //spriteBatch.DrawString(_gameFont, "// TODO", _playerPosition, Color.Green);
+        }
 
-            //spriteBatch.End();
+        /// <summary>
+        /// Method that handles the collision interactions of the sprites and any actions that result from those collisions
+        /// </summary>
+        private void AsteroidDetectionAndUpdates()
+        {
+            //variables for tracking blaster fires and asteroids that need to be removed
+            int toRemoveAsteroid = -1;
+            int toRemoveBlaster = -1;
+            for (int i = 0; i < asteroids.Count; i++)
+            {
+                //If the space ship has hit an asteroid, the ship will be destroyed
+                if (ship.CollidesWith(asteroids[i].Bounds) && !ship.Destroyed)
+                {
+                    ship.Destroyed = true;
+                    CreateExplosion(new Vector2(ship.Bounds.X, ship.Bounds.Y));
+                    gameLose = true;
+                }
+                foreach (var fire in blasterFire)
+                {
+                    //If a blaster fire has hit an asteroid, that fire will be gone along with the asteroid
+                    if (fire.CollidesWith(asteroids[i].Bounds) && !asteroids[i].Destroyed)
+                    {
+                        asteroids[i].Destroyed = true;
+                        CreateExplosion(new Vector2(asteroids[i].Bounds.Center.X, asteroids[i].Bounds.Center.Y));
+                        toRemoveAsteroid = asteroids.IndexOf(asteroids[i]);
+                        toRemoveBlaster = blasterFire.IndexOf(fire);
+                    }
+                }
+                for (int j = i + 1; j < asteroids.Count; j++)
+                {
+                    //Detects collision between asteroids, taken from PhysicsExampleC assignment by Nathan Bean
+                    if (asteroids[i].CollidesWith(asteroids[j]))
+                    {
+                        asteroids[i].Colliding = true;
+                        asteroids[j].Colliding = true;
 
+                        Vector2 collisionAxis = asteroids[i].Center - asteroids[j].Center;
+                        collisionAxis.Normalize();
+                        float angle = (float)System.Math.Acos(Vector2.Dot(collisionAxis, Vector2.UnitX));
+                        float m0 = asteroids[i].Mass;
+                        float m1 = asteroids[j].Mass;
+
+                        Vector2 u0 = Vector2.Transform(asteroids[i].Velocity, Matrix.CreateRotationZ(angle));
+                        Vector2 u1 = Vector2.Transform(asteroids[j].Velocity, Matrix.CreateRotationZ(angle));
+
+                        Vector2 v0;
+                        Vector2 v1;
+
+                        v0.X = ((m0 - m1) / (m0 + m1)) * u0.X + ((2 * m1) / (m0 + m1)) * u1.X;
+                        v1.X = ((2 * m0) / (m0 + m1)) * u0.X + ((m1 - m0) / (m0 + m1)) * u1.X;
+                        v0.Y = u0.Y;
+                        v1.Y = u1.Y;
+
+                        asteroids[i].Velocity = Vector2.Transform(v0, Matrix.CreateRotationZ(angle));
+                        asteroids[i].Velocity = Vector2.Transform(v1, Matrix.CreateRotationZ(angle));
+                    }
+                }
+            }
+            //Removes the blaster fire and asteroid sprites if they collided
+            if (toRemoveAsteroid != -1 && toRemoveBlaster != -1)
+            {
+                asteroids[toRemoveAsteroid] = null;
+                asteroids.RemoveAt(toRemoveAsteroid);
+                blasterFire[toRemoveBlaster] = null;
+                blasterFire.RemoveAt(toRemoveBlaster);
+            }
+            //The player wins if there are no more asteroids
+            if (asteroids.Count == 0) gameVictory = true;
+        }
+
+        /// <summary>
+        /// Helper method for creating new blaster fire sprites
+        /// </summary>
+        private void CreateBlasterFire()
+        {
+            BlasterFireSprite newFire = new BlasterFireSprite(ship.Angle, ship.Position,
+                new BoundingRectangle(ship.Position.X - 16, ship.Position.Y - 16, 16, 16));
+            newFire.LoadContent(_content);
+            blasterFire.Add(newFire);
+        }
+
+        /// <summary>
+        /// Helper method for creating new explosion sprites
+        /// </summary>
+        /// <param name="position">The position of the explosion</param>
+        private void CreateExplosion(Vector2 position)
+        {
+            ExplosionSprite newExplosion = new ExplosionSprite(position);
+            newExplosion.LoadContent(_content);
+            explosions.Add(newExplosion);
         }
     }
 }
